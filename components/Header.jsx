fb1eef7265275a2cbf3296d9161df0acc5e70cfc
ebbilge <44533394+ebbilge@users.noreply.github.com>
2022-07-19 18:49:44 +0300

    &apos; update

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
         <div className='border-b w-full inline-block border-blue-400 py-8'>
             <div className='md:float-left block'>
                 <Link href='/'>
-                    <span className='cursor-pointer font-bold text-4xl text-white '>Aech's Paracosm </span>
+                    <span className='cursor-pointer font-bold text-4xl text-white '>Aech&apos;s Paracosm </span>
                 </Link>
             </div>
             <div className='hidden md:float-left md:contents'>
